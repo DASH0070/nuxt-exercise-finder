489bf5fb821df31d87cc8b041a36dd541242811f
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-17 13:31:03 +0530

    remove unncesary nodes

--- components/ExerciseList.vue
+++ components/ExerciseList.vue
@@ -5,17 +5,16 @@
             <p>{{counter + ' of ' + Math.ceil(exerciseList.length / 10 )}}</p>
             <button @click="counter++"> + </button>
         </div>
-        <Disclosure v-for="exercise in exerciseList.slice((counter - 1) * 10, (counter - 1) * 10 + 10)">
+        <template v-for="exercise in exerciseList.slice((counter - 1) * 10, (counter - 1) * 10 + 10)">
             <NuxtLink :to="exercise" v-if="exercise"
                 class="flex w-full justify-between rounded-lg bg-purple-100 px-4 my-1 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>{{exercise}}</span>
             </NuxtLink>
-        </Disclosure>
+        </template>
     </div>
 </template>
   
 <script setup lang="ts">
-import { Disclosure } from '@headlessui/vue';
 const props = defineProps<{ exerciseList: string[] }>();
 const counter = ref(1);
 
